4d19f94191a755c308b4315540bd5bc806ec2be8
Kyousei <luascriptingtut@gmail.com>
2017-10-11 00:30:24 +0200

    Added ;update and ;rollslots
    Now you can update your bot by doing `;update`!
    Also added `;rollslots`, because other versions had it and this didn't :smile:

--- YotsugiMain.py
+++ YotsugiMain.py
@@ -770,6 +770,38 @@ async def user(ctx, *, member: discord.Member):
 
 '''---------------------------------------------------------------------'''
 
+@client.command(pass_context = True, aliases=['slotroll'])
+async def rollslots(ctx):
+        """ Roll the slot machine """
+        emojis = "🍎🍊🍐🍋🍉🍇🍓🍒"
+        a = random.choice(emojis)
+        b = random.choice(emojis)
+        c = random.choice(emojis)
+
+        embed = discord.Embed(description = "Congratulations, You won!")
+        if (a == b == c):
+            message = 'and won! 🎉'
+        elif (a == b) or (a == c) or (b == c):
+            message = 'and almost won (2/3)'
+        else:
+            message = 'and lost...'
+
+
+        embed = discord.Embed(description = f"**{ctx.message.author.mention}**, you rolled the slots...\n**〖 {a} 〗〖 {b} 〗〖 {c} 〗**\n{message}", color = embed_color)
+        await client.say(embed = embed)
+	
+
+@client.command(pass_context = True)
+async def update(ctx):
+    updatefile = "linuxUPDATE.sh"
+    if ctx.message.author.id != owner:
+        embed = discord.Embed(description = "You're not the owner!", color = 0xFF0000)
+        await client.say(embed = embed)
+    else:
+        embed = discord.Embed(description = "Updating...", color = embed_color)
+        await client.say(embed = embed)
+        os.startfile(updatefile)
+
+
+
 client.run(BotToken)
-
-
