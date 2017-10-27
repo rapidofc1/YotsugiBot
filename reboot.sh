1f4ca1af9143e95610e60132b54ec6e0abcdf0a2
Kyousei <luascriptingtut@gmail.com>
2017-10-27 17:06:34 +0200

    Update reboot.sh

--- reboot.sh
+++ reboot.sh
@@ -1,20 +1,10 @@
 echo "Reboot initiated.."
 sleep 5
-echo "Killed the bot. Welcome to the 'after-reboot' menu!"
-echo "Please select what you'd like to do."
+echo "Killed the bot."
+echo "Rebooting..."
+sleep 2
+echo ""
+python3 YotsugiMain.py
 
-
-choice=2
-		echo "1-  Reboot"
-		echo "2-  Exit"
-		
-while [ $choice -eq 2 ]; do
-read choice
-if [ $choice -eq 1 ] ; then
-		echo "Reboot selected!"
-		sleep 1
-		echo "Rebooting..."
-		bash linuxRUN.sh
-	fi
 done
 exit 0
