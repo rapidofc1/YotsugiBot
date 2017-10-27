170026c0d0d653e21b29df43e864626b3ff0a8be
Kyousei <luascriptingtut@gmail.com>
2017-10-27 17:26:17 +0200

    Update YotsugiMain.py

--- YotsugiMain.py
+++ YotsugiMain.py
@@ -690,7 +690,7 @@ async def on_server_role_create(role, channel = loggingchannel):
     	embed.add_field(name="Server ID:", value=message.server.id, inline=False)
     	await client.send_message(discord.Object(id=loggingchannel), embed=embed)
     else:
-	print("")
+		print("")
     await client.process_commands(role)
 
 
@@ -703,7 +703,7 @@ async def on_server_role_delete(role, channel = loggingchannel):
     	embed.add_field(name="Server ID:", value=message.server.id, inline=False)
     	await client.send_message(discord.Object(id=loggingchannel), embed=embed)
     else:
-	print("")
+		print("")
     await client.process_commands(role)
 
 
@@ -717,7 +717,7 @@ async def on_member_ban(member, channel = loggingchannel):
     	embed.add_field(name="Server ID:", value=message.server.id, inline=False)
     	await client.send_message(discord.Object(id=loggingchannel), embed=embed)
     else:
-	print("")
+		print("")
     await client.process_commands(member)
 
 
@@ -732,7 +732,7 @@ async def on_message_edit(message, after, channel = loggingchannel):
     	embed.add_field(name="Server ID:", value=message.server.id, inline=False)
     	await client.send_message(discord.Object(id=loggingchannel), embed = embed)
     else:
-	print("")
+		print("")
     await client.process_commands(message)
 
 
@@ -746,7 +746,7 @@ async def on_message_delete(message, channel = loggingchannel):
     	embed.add_field(name="Server ID:", value=message.server.id, inline=False)
     	await client.send_message(discord.Object(id=loggingchannel), embed = embed)
     else:
-	print("")
+		print("")
     await client.process_commands(message)
 
 ##### LOGGING #####
