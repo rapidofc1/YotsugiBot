a19f78f7c4a85d826da8930a62976dd2717d4c5e
Kyousei <luascriptingtut@gmail.com>
2017-10-28 12:15:24 +0200

    mou ichi dake :tm:

--- YotsugiMain.py
+++ YotsugiMain.py
@@ -809,7 +809,7 @@ async def updatelin(ctx):
         embed = discord.Embed(description = "Updating...", color = embed_color)
         await client.say(embed = embed)
         os.startfile(updatefile)
-	await client.logout()
+        await client.logout()
 	
 
 @client.command(pass_context = True)
