a14852540adf9ada12ee7e9cac29603dd452d8a0
Kyousei <luascriptingtut@gmail.com>
2017-10-28 11:34:30 +0200

    Update linuxUPDATE.sh

--- linuxUPDATE.sh
+++ linuxUPDATE.sh
@@ -1,23 +1,10 @@
 #!/bin/sh
-echo "Welcome to YotsugiBot installer!"
-echo "	Please choose what you'd like to do below!"
 echo ""
 echo ""
-
-
-choice=1
-		echo "1. Download Yotsugi"
-	
-while [ $choice -eq 1 ]; do
-read choice
-if [ $choice -eq 1 ] ; then
-
-		echo ""
-		echo "Downloading Yotsugi....."
-		curl -L https://raw.githubusercontent.com/Kyousei/YotsugiBotLinuxTest/master/testfile.sh | sh
-		echo ""
-		echo "Downloaded!"
-		
-	fi
+echo ""
+echo "Downloading Yotsugi....."
+curl -L https://raw.githubusercontent.com/Kyousei/YotsugiBotLinuxTest/master/testfile.sh | sh
+echo ""
+echo "Downloaded!"
 done
 exit 0
