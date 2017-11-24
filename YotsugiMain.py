44447234b1a92e64250aee0771cfa9acc085732a
Kyousei <luascriptingtut@gmail.com>
2017-11-24 19:21:34 +0100

    Possible fix?

--- YotsugiMain.py
+++ YotsugiMain.py
@@ -793,7 +793,7 @@ async def on_server_role_create(role, channel = loggingchannel):
     	embed.add_field(name="Server ID:", value=message.server.id, inline=False)
     	await client.send_message(discord.Object(id=loggingchannel), embed=embed)
     else:
-	return
+	pass
     await client.process_commands(role)
 
 
@@ -806,7 +806,7 @@ async def on_server_role_delete(role, channel = loggingchannel):
     	embed.add_field(name="Server ID:", value=message.server.id, inline=False)
     	await client.send_message(discord.Object(id=loggingchannel), embed=embed)
     else:
-	return
+	pass
     await client.process_commands(role)
 
 
@@ -820,7 +820,7 @@ async def on_member_ban(member, channel = loggingchannel):
     	embed.add_field(name="Server ID:", value=message.server.id, inline=False)
     	await client.send_message(discord.Object(id=loggingchannel), embed=embed)
     else:
-	return
+	pass
     await client.process_commands(member)
 
 
@@ -835,7 +835,7 @@ async def on_message_edit(message, after, channel = loggingchannel):
     	embed.add_field(name="Server ID:", value=message.server.id, inline=False)
     	await client.send_message(discord.Object(id=loggingchannel), embed = embed)
     else:
-	return
+	pass
     await client.process_commands(message)
 ##### LOGGING #####
 
