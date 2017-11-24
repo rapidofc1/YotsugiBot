bdbba2ff8a842bb650740c56a9c7fdc7bc73c0b6
Kyousei <luascriptingtut@gmail.com>
2017-11-24 19:45:49 +0100

    Not needed on Windows.

--- linuxUPDATE.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-echo ""
-echo ""
-echo ""
-echo "Downloading Yotsugi....."
-curl -L https://raw.githubusercontent.com/Kyousei/YotsugiBotLinuxTest/master/testfile.sh | sh
-echo ""
-echo "Downloaded!"
-python3 YotsugiMain.py
