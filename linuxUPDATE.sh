566ce791f5d04fe0f4795fd2f9168e859dabfb46
Kyousei <luascriptingtut@gmail.com>
2017-11-24 19:46:37 +0100

    Fixed. Should work properly?

--- linuxUPDATE.sh
+++ linuxUPDATE.sh
@@ -1,9 +1,25 @@
 #!/bin/sh
+echo "Welcome to YotsugiBot installer!"
+echo "	Please choose what you'd like to do below!"
 echo ""
 echo ""
-echo ""
-echo "Downloading Yotsugi....."
-curl -L https://raw.githubusercontent.com/Kyousei/YotsugiBotLinuxTest/master/testfile.sh | sh
-echo ""
-echo "Downloaded!"
-python3 YotsugiMain.py
+
+
+choice=1
+		echo "1. Download Yotsugi"
+	
+while [ $choice -eq 1 ]; do
+read choice
+
+if [ $choice -eq 1 ] ; then
+
+		echo ""
+		echo "Downloading Yotsugi....."
+		curl -L https://raw.githubusercontent.com/Kyousei/YotsugiBotLinuxTest/master/testfile.sh | sh
+		echo ""
+		echo "Downloaded!"
+		
+fi
+
+done
+exit 0
